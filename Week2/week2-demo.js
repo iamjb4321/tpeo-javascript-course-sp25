9f5e422f5ce6fcaf8cb1ee63521261821fd87c55
meli-huang <meli.huang05@gmail.com>
2025-02-25 13:59:17 -0600

    Added week1 demo + prep for week2

---
+++ Week2/week2-demo.js
@@ -0,0 +1,13 @@
+// WEEK 2 DEMO: Values, Variables, Functions
+// Task: Write a function to calculate the area of a triangle
+
+
+// Write function here
+
+
+// Define some variables to test it here
+
+
+// Call your function here!
+
+

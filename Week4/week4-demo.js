ab3e7a00a9fb6c822bc71455939745be3227448f
iamjb4321 <137758634+iamjb4321@users.noreply.github.com>
2025-03-11 22:08:07 -0500

    week 4

--- Week4/week4-demo.js
+++ Week4/week4-demo.js
@@ -12,6 +12,17 @@ var currentTime = 1; // global variable
 
 function printNumber()
 {
+    //do the parameters have to have hte same name as the method setTimeout is affecting. 
+    // If so, does the order of hte parameters matter?
+    // clear interval, clear timeout, setInterval, setInterval
     console.log(currentTime);
     currentTime++;
 }
+
+//ues a ninterval to count each second
+var intervalID = setInterval(printNumber, 1000);
+
+//limit interval to 15 seconds
+//clearInterval
+setTimeout(clearInterval, 16000, intervalID);
+
--- Week4/week4-homework.js
+++ Week4/week4-homework.js
@@ -7,11 +7,18 @@ power of three prints. Use a timeout to call this function after waiting 3 secon
 
 function powersOfThree() {
     // Write your code here
-
+    for(i = 0; i <= 9; i++){
+        var num = Math.pow(3, i);
+        if(num == 9){
+            console.log(num + " -- This is my favorite power of three!");
+        }else{
+            console.log(num);
+        }
+    }
 }
 
 // Call your function here
-
+setTimeout(powersOfThree, 3000);
 /*
 Question 2: Write a function named everyTenSeconds that prints out “Hello World!” to the terminal
 every 10 seconds for a total of 2 minutes. Feel free to create any helper functions you need to
@@ -20,7 +27,9 @@ accomplish this. Test your function by calling it once.
 
 function everyTenSeconds() {
     // Write your code here
-
+    var intervalId = setInterval(console.log, 10000, "Hello World");
+    setTimeout(clearInterval, 130000, intervalId);
 }
 
 // Call your function here
+everyTenSeconds();

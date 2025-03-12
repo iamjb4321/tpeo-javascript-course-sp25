1b868a70dc47343e99293a9ff317225f7539aa6b
iamjb4321 <137758634+iamjb4321@users.noreply.github.com>
2025-03-11 19:40:32 -0500

    week3 demo merge

--- Week3/week3-demo.js
+++ Week3/week3-demo.js
@@ -4,3 +4,24 @@ Task: write a function, walkToTreasure, that takes in x and y coordinates on a b
 and “walks” to the location of the treasure buried in the sand. 
 */
 
+function walkToTreasure(x,y, treasureX, treasureY){
+
+    //find x coordinate
+    x = getToPosition(x, treasureX)
+    y = getToPosition(y, treasureY)
+    console.log("Found treasure");
+}
+
+function getToPosition(z, treasurePos){
+    while(z != treasurePos){
+        if(z < treasurePos){
+            console.log("Going right!")
+            z++;
+        }else{
+            console.log("Going left!")
+            z--;
+        }
+    }
+}
+
+walkToTreasure(0, 0, 3, 9);

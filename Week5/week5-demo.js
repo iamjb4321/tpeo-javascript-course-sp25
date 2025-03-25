01e2ae5c2daeae480ee8ea8fd4c47c44e214910c
meli-huang <meli.huang05@gmail.com>
2025-03-25 10:41:55 -0500

    pushing week4 hw and week5 prep

---
+++ Week5/week5-demo.js
@@ -0,0 +1,11 @@
+// WEEK 5 DEMO: 
+/* 
+Let's create a constructor for a Student, which takes in their first name, 
+last name, university, major, and graduation year. 
+*/ 
+
+
+/*
+Now, create a constructor for a Course, which will take in a student taking the class, 
+the class name, and the class location. 
+*/ 
---
+++ Week5/week5-homework.js
@@ -0,0 +1,18 @@
+// WEEK 5 HOMEWORK: 
+/*
+Question 1: Create a constructor for a Car object. Each Car object should contain the 
+following properties: model, year, color, and mpg. Use the constructor to create 3 different 
+Car objects. Print out the mpg of each car to the console. 
+*/
+
+// Your code here! 
+
+/*
+Question 2: Create a constructor for a Vehicle object. Each Vehicle object should contain only 
+one property: car. Using the 3 Car objects you created previously, create 3 Vehicle objects. 
+Print out the mpg of the car property in each Vehicle object.
+*/
+
+// Your code here! 
+
+

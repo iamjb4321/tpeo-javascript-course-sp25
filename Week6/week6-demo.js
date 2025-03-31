c977b1d9be31c70d794e4315f27eda7a863ac9c3
meli-huang <meli.huang05@gmail.com>
2025-03-31 16:00:27 -0500

    pushing week5 hw and week6 prep

---
+++ Week6/week6-demo.js
@@ -0,0 +1,36 @@
+/*
+Let’s practice working with strings and arrays by writing a few different functions:
+truncate(string, number): In this function, we want to cut off the last number
+characters of string and return that new string
+*/
+
+function truncate(string, number) {
+    // Write code below
+}
+
+console.log(truncate("Hello World!", 1));
+console.log(truncate("JavaScript", 6));
+console.log(truncate("TPEO", 4));
+
+/*
+capitalizeFirstLetters(strings): In this function, we want to capitalize the first
+letter of each string in strings and return the updated array
+*/
+
+function capitalizeFirstLetters(strings) {
+    // Write code below
+}
+
+console.log(capitalizeFirstLetters(["sarah", "melissa", "krish"]));
+
+
+/*
+swapcase(string): In this function, we want to edit a string so that its lowercase
+letters become uppercase and so that its uppercase letters become lowercase
+*/
+
+function swapcase(string) {
+    // Write code below
+}
+
+console.log(swapcase("aBcDefgHIJ"));
---
+++ Week6/week6-homework.js
@@ -0,0 +1,31 @@
+/*
+Question 1: Write a function, reverse, that accepts one parameter: string.
+In the function, return the string but reversed (Example: if “hey” is the
+input, then return “yeh”). Call this function three times each with
+different inputs and print the results to the terminal.
+*/
+
+function reverse(string) {
+    // Write code below
+}
+
+/*
+Question 2: Write a function, numVowels, that accepts one parameter: string.
+In the function, return the number of vowels string has. Call this function
+three times each with different inputs and print the results to the terminal.
+*/
+
+function numVowels(string) {
+    // Write code below
+}
+
+/*
+Question 3: Write a function, numStringsShorterThanLength that accepts two parameters:
+strings (an array of strings) and length (a number). In the function, return the number
+of strings in strings that have a length less than length. Call this function three times
+each with different inputs and print the results to the terminal.
+*/
+
+function numStringsShorterThanLength(strings, length) {
+    // Write code below
+}

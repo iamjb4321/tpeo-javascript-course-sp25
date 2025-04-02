76560c9abe6e189edafffc1fb2fe4d9c9019f4d1
iamjb4321 <137758634+iamjb4321@users.noreply.github.com>
2025-04-01 19:44:50 -0500

    joel's week 5 demo & homework

--- Week5/week5-demo.js
+++ Week5/week5-demo.js
@@ -6,21 +6,19 @@ last name, university, major, and graduation year.
 
 function Student(firstName, lastName, university, major, graduationYear) {
     this.firstName = firstName;
-    this.lastName = lastName; 
+    this.lastName = lastName;
     this.university = university;
     this.major = major;
-    this.graduationYear = graduationYear; 
-    this.hasGraduated = graduationYear <= 2024; 
+    this.graduationYear = graduationYear;
+    this.hasGraduated = graduationYear <= 2024;
 }
 
 var sarah = new Student("Sarah", "Ding", "UT Austin", "Informatics", 2027);
-var vincent = new Student("Vincent", "Do", "UT Austin", "Computer science", 2025);
+var vincent = new Student("Vincent", "Do", "UT Austin", "Computer Science", 2025);
 
 console.log(sarah.major);
 console.log(vincent["hasGraduated"]);
-console.log(`${vincent.firstName} ${vincent.lastName} is studying ${vincent["major"]} until ${vincent.graduationYear}.`);
-
-
+console.log(`${vincent.firstName} ${vincent.lastName} is studying ${vincent["major"]} until ${vincent.graduationYear}.`)
 /*
 Now, create a constructor for a Course, which will take in a student taking the class, 
 the class name, and the class location. 
@@ -31,6 +29,7 @@ function Course(student, name, location) {
     this.name = name;
     this.location = location;
 }
+
 
 var introToJS = new Course(vincent, "Intro to Javascript", "GDC 2.210");
 console.log(introToJS.student.graduationYear);
--- Week5/week5-homework.js
+++ Week5/week5-homework.js
@@ -6,6 +6,19 @@ Car objects. Print out the mpg of each car to the console.
 */
 
 // Your code here! 
+function Car(model, year, color, mpg){
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.mpg = mpg;
+}
+
+var car1 = new Car("Nissan Altima", "2015", "Black", "20 mpg");
+var car2 = new Car("Dodge Charger", "2023", "Red", "15 mpg");
+var car3 = new Car("Porche Cayenne", "2018", "White", "18 mpg");
+console.log(car1.mpg);
+console.log(car2.mpg);
+console.log(car3.mpg);
 
 /*
 Question 2: Create a constructor for a Vehicle object. Each Vehicle object should contain only 
@@ -14,5 +27,13 @@ Print out the mpg of the car property in each Vehicle object.
 */
 
 // Your code here! 
-
-
+function Vehicle(car){
+    this.car = car;
+}
+ 
+var vehicle1 = new Vehicle(car1);
+var vehicle2 = new Vehicle(car2);
+var vehicle3 = new Vehicle(car3);
+console.log(vehicle1.car.mpg);
+console.log(vehicle2.car.mpg);
+console.log(vehicle3.car.mpg);

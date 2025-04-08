9a712eb81e10a8249c1372975933f27eca7021c4
meli-huang <meli.huang05@gmail.com>
2025-04-08 16:51:59 -0500

    pushing week6 hw and week7 prep

---
+++ Week7/week7-homework.js
@@ -0,0 +1,17 @@
+/*
+Question 1: Use a for-loop to make a 2D array of Car objects that is 10 rows by 10 columns. 
+Each Car object should have the following properties: model, color, year, mpg, and id. 
+Each Car object should have the same values for model, color, year, and mpg but different values 
+for id that increment with each Car object made. For example, the first car made should have id = 1, 
+the second car made should have id = 2, etc.
+*/
+
+
+/*
+Question 2: Write a function getCarsBetweenIds that takes two parameters: idMin and idMax. 
+Return a new array of the Car objects that have an id between idMin and idMax (idMin <= id <= idMax). 
+Before returning the array, change all the Car objects’ color property to “blue”. Test your function 
+by calling it 3 times and printing the results to the terminal.
+*/
+
+
